1b8ff185c8734dfc09e84a6d57bff48903d06938
MahajanNeha26 <50761125+MahajanNeha26@users.noreply.github.com>
2020-09-12 16:31:06 +0530

    Delete Program1.js

--- Program1.js
+++
@@ -1,6 +0,0 @@
-let txt = "Somesh went to School";
-let abc = txt.search("o");
-document.getElementById("demo").innerHTML = "<b>OUTPUT :</b>" + abc; 
-/*OR*/
-let xyz = txt.indexOf ("e",4);
-document.getElementById("demo1").innerHTML = "<b>OUTPUT :</b>" + xyz; 
